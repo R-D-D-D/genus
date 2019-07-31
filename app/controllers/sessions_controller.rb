af2bceee31f87196373ccfcca582d5da8cd33fd4
R-D-D-D <wangrunding@gmail.com>
2019-07-31 23:22:21 +0800

    RD: add necessary links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,28 +4,28 @@ class SessionsController < ApplicationController
   def new
   end
 
-    def create
-        user = User.find_by(email: params["email"]).try(:authenticate, params["password"])
-        if user 
-          session[:user_id] = user.id
-          render json: {
-              status: :created,
-              logged_in: true,
-              user: user
-          }
-        else
-          render json: { status: 401 }
-        end
+  def create
+    user = User.find_by(email: params["email"]).try(:authenticate, params["password"])
+    if user
+      session[:user_id] = user.id
+      render json: {
+          status: :created,
+          logged_in: true,
+          user: user
+      }
+    else
+      render json: {status: 401}
     end
+  end
 
-    def logged_in
-      if @current_user
-        render json: {
+  def logged_in
+    if @current_user
+      render json: {
           logged_in: true,
           user: @current_user
-        }
-      else
-        render json: {
+      }
+    else
+      render json: {
           logged_in: false
       }
     end
@@ -33,6 +33,6 @@ class SessionsController < ApplicationController
 
   def logout
     reset_session
-    render json: { status: 200, logged_out: true }
+    render json: {status: 200, logged_out: true}
   end
 end

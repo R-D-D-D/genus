6e039363885f500b414b7efe3a0d99d8306c8324
mannggoo <yuhuilim1999@gmail.com>
2019-08-03 23:24:35 +0800

    YH: Minor Changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_040725) do
+ActiveRecord::Schema.define(version: 2019_07_27_161921) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
